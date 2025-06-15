5370bef97b2308cd91ccae66ba98b2f909cffab5
Ramesh Jana <81910860+Ramesh2911@users.noreply.github.com>
2025-06-15 17:41:29 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: 3308,
+    // port: 3308,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
